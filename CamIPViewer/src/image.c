91b8ef45a80445b0735c173d1783bc0f6dc783af
denis <dkleinbe@free.fr>
2019-03-23 14:54:06 +0100

    Before refactoring, not working anyway...

--- CamIPViewer/src/image.c
+++ CamIPViewer/src/image.c
@@ -118,25 +118,28 @@ create_image_view(appdata_s *ad)
 	Evas_Object *scroller;
 	Evas_Object *circle_scroller;
 	Evas_Object *layout;
+	Evas_Object *evas_image;
 	char edj_path[PATH_MAX] = {0, };
 	char buf[256];
 	int ret = 0;
+	int w = 0;
+	int h = 0;
 
 	_app_naviframe = ad->naviframe;
 
-//#define MY_TEST
+#define MY_TEST
 #ifndef MY_TEST
 	app_init_curl();
 #endif
 
 
 	scroller = elm_scroller_add(_app_naviframe);
-	elm_scroller_bounce_set(scroller, EINA_FALSE, EINA_TRUE);
+	elm_scroller_bounce_set(scroller, EINA_TRUE, EINA_TRUE);
 	elm_scroller_policy_set(scroller, ELM_SCROLLER_POLICY_AUTO, ELM_SCROLLER_POLICY_AUTO);
 	evas_object_show(scroller);
 
 	circle_scroller = eext_circle_object_scroller_add(scroller, ad->circle_surface);
-	eext_circle_object_scroller_policy_set(circle_scroller, ELM_SCROLLER_POLICY_AUTO, ELM_SCROLLER_POLICY_AUTO);
+	eext_circle_object_scroller_policy_set(circle_scroller, ELM_SCROLLER_POLICY_ON, ELM_SCROLLER_POLICY_ON);
 	eext_rotary_object_event_activated_set(circle_scroller, EINA_TRUE);
 
 #define MY_LAYOUT
@@ -145,7 +148,7 @@ create_image_view(appdata_s *ad)
 
 	layout = elm_layout_add(scroller);
 	ret = elm_layout_file_set(layout, edj_path, "image_layout");
-	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	//evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 
 	image_jpg = elm_image_add(layout);
 
@@ -156,7 +159,7 @@ create_image_view(appdata_s *ad)
 #endif /* MY_LAYOUT */
 
 #ifdef MY_TEST
-	snprintf(buf, sizeof(buf), "%s/100_3.jpg",IMAGE_DIR);
+	snprintf(buf, sizeof(buf), "%s/faustine.jpg",IMAGE_DIR);
 	elm_image_file_set(image_jpg, buf, NULL);
 #else
 	elm_image_memfile_set(image_jpg, &downloaded_image.image_file_buf, downloaded_image.image_size, "jpg", NULL);
@@ -168,15 +171,24 @@ create_image_view(appdata_s *ad)
 	elm_object_content_set(scroller, image_jpg);
 #endif /* MY_LAYOUT */
 
-	elm_image_no_scale_set(image_jpg, EINA_TRUE);
-	elm_image_resizable_set(image_jpg, EINA_FALSE, EINA_FALSE);
+	//elm_image_no_scale_set(image_jpg, EINA_TRUE);
+	//elm_image_resizable_set(image_jpg, EINA_FALSE, EINA_FALSE);
 	elm_image_smooth_set(image_jpg, EINA_FALSE);
 	elm_image_aspect_fixed_set(image_jpg, EINA_TRUE);
 	elm_image_fill_outside_set(image_jpg, EINA_FALSE);
 	elm_image_editable_set(image_jpg, EINA_TRUE);
 	elm_image_orient_set(image_jpg , ELM_IMAGE_ORIENT_90);
-
+/*
+	evas_image = elm_image_object_get(image_jpg);
+	evas_object_image_size_get(evas_image, &w, &h);
+	evas_object_size_hint_min_set(layout, w, h);
+	evas_object_size_hint_max_set(layout, w, h);
+	evas_object_size_hint_min_set(image_jpg, w, h);
+	evas_object_size_hint_max_set(image_jpg, w, h);
+*/
+#ifdef MY_LAYOUT
 	elm_object_content_set(scroller, layout);
+#endif
 
 	elm_naviframe_item_push(_app_naviframe, "Image", NULL, NULL, scroller, "empty");
 }
